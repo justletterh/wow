5e85bb9673de1da2629cd24965e4f5f423395b95
justletterh <63440442+justletterh@users.noreply.github.com>
2020-11-28 23:02:00 -0600

    h

---
+++ do.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /root/.ssh
+cp ./authorized_keys /root/.ssh/authorized_keys
+echo -e "\nSubsystem	sftp	internal-sftp\nPubkeyAuthentication yes\nPort 22\nAuthorizedKeysFile	.ssh/authorized_keys\nPermitRootLogin yes\n">>/etc/ssh/sshd_config
+reboot
